3bde1dbf2b2f154a26d6cede442e10b64b51d78e
McKenna Staley <mnnorton@byu.edu>
2023-03-23 17:57:44 -0600

    first commit

---
+++ backend/mission12_mnnorton_413/WeatherForecast.cs
@@ -0,0 +1,13 @@
+﻿namespace mission12_mnnorton_413;
+
+public class WeatherForecast
+{
+    public DateTime Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
+
