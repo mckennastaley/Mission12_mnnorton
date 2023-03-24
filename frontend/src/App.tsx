256108ce962eb8c590b23c09551ee0f622370697
McKenna Staley <mnnorton@byu.edu>
2023-03-23 18:37:03 -0600

    let's pray this works

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,63 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Card from 'react-bootstrap/Card';
+import data from '/Users/mckennastaley/Documents/School/Winter 2023/IS 413/Mission12_mnnorton/frontend/src/CollegeBasketballTeams.json';
+import '/Users/mckennastaley/Documents/School/Winter 2023/IS 413/Mission12_mnnorton/frontend/src/team.css';
+
+interface ITeamProps {
+  school: string;
+  name: string;
+  city: string;
+  state: string;
+}
+
+const teamJSON = data;
 
 function Welcome(){
-  return(<h1>The best Mucisians</h1>);
+  return(<h1>Welcome! View NCAA March Madness Information here.</h1>);
+}
+
+class Team extends React.Component<ITeamProps> {
+  render() {
+    const { school, name, city, state } = this.props;
+    
+    return (
+      <Card className="team-card">
+        <Card.Body>
+          <Card.Title>{school}</Card.Title>
+          <Card.Subtitle className="mb-2 text-muted">{name}</Card.Subtitle>
+          <Card.Text>
+            {city}, {state}
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    );
+  }
+}
+
+function TeamList() {
+  return (
+    <div>
+      {teamJSON.teams.map(oneTeam => <Team
+        key={oneTeam.tid}
+        school={oneTeam.school}
+        name={oneTeam.name}
+        city={oneTeam.city}
+        state={oneTeam.state}
+      />)}
+    </div>
+  );
 }
 
 
 function App() {
-  
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return(
+    <div>
+      <Welcome/>
+      <TeamList/>
     </div>
-  );
+  )
 }
 
 export default App;
